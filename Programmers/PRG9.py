cbaf9380fe9b33692eabfcf66422159294362315
jeongmun <lot9616@gmail.com>
2022-04-18 17:40:26 +0900

    PRG9

---
+++ Programmers/PRG9.py
@@ -0,0 +1,19 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/42898
+'''
+
+def solution(m, n, puddles):
+    puddles = [[q,p] for p, q in puddles]
+    grid = [[ 0 for j in range(m + 1)] for i in range(n + 1)]
+    grid[1][1] = 1
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+
+            if i == 1 and j == 1:
+                continue
+
+            if [i, j] in puddles:
+                grid[i][j] = 0
+            else:
+                grid[i][j] = (grid[i - 1][j] + grid [i][j - 1]) % 1000000007
+    return grid[n][m]
